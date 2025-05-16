729793273650e3ab14f59778dbfa54eb511eebcf
captainkoopa420 <adamswilliam905@gmail.com>
2025-05-15 20:15:38 -0700

    Update lite_demo.py

--- verdant-lite/examples/lite_demo.py
+++ verdant-lite/examples/lite_demo.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from core.MemorySystem import MemorySystem
 from core.SubconsciousProcessor import SubconsciousProcessor
 from core.ConsciousAgent import ConsciousAgent
